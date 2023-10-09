7c777485d84c2855b8dbe7132515ec2da0862121
Daniel Brandt <dbbrandt@gmail.com>
2023-10-09 12:46:37 -0400

    Initial Commit for Dev branch

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Flex} from "@aws-amplify/ui-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Flex direction="column">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Flex>
 );
 
 // If you want to start measuring performance in your app, pass a function
